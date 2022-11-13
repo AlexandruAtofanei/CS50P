b5be53e29f0b469b31f1d055a8d75826b1216533
AlexandruAtofanei <49373550+AlexandruAtofanei@users.noreply.github.com>
2022-11-13 18:08:19 +0200

    Create einstein.py

---
+++ Week 0 Functions. Variables/einstein.py
@@ -0,0 +1,16 @@
+def main():
+
+    # Ask user for his mass (kilograms)
+    m = int(input("what is your weight in kilograms? "))
+
+    # Define c - speed of light
+    c = 300000000
+
+    # Calculate E=mc2
+    E = m * c ** 2
+    # E= m * (300000000 ** 2)
+
+    # Print the result
+    print(E)
+
+main ()
