4eb196f7bb31eb4a17d2979de8988741d6de651b
AlexandruAtofanei <49373550+AlexandruAtofanei@users.noreply.github.com>
2022-11-13 18:44:22 +0200

    Create emojize.py

---
+++ Week 4 Libraries/emojize.py
@@ -0,0 +1,11 @@
+# Installing the 3rd party library
+# pip install emoji
+
+# Importing the downloaded and installed library,\; function emojize
+from emoji import emojize
+
+# Ask user for input
+ask = input("Tell me your code? ")
+
+# Print the emojized input
+print(emojize(ask))
