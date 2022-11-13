931e7fad728e14adbca3262cdb303e4ad5d49a96
AlexandruAtofanei <49373550+AlexandruAtofanei@users.noreply.github.com>
2022-11-13 18:06:02 +0200

    Create playback.py

---
+++ Week 0 Functions. Variables/playback.py
@@ -0,0 +1,8 @@
+def main():
+    
+    # Ask user for input and replace space with ...
+    ask = input("What`s up? ").replace(" ", "...")
+    # Print the input replacing space with ...
+    print(ask)
+
+main()
